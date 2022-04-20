1be34bf909dd2ec85a655d8ec11df623363d5e8d
Jorgeml15 <103546643+Jorgeml15@users.noreply.github.com>
2022-04-20 11:13:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,25 +13,41 @@ string codeData="";
 
 int main(int argc, char** argv) {
     
-    string filename; // holds name of input file
+    string inputFile, outputFile; // holds name of input and output files
 
     bool running = true;
     while(running){
         
-    // collect input file
-    cout<<"Enter the input file name\n";
-    cin>>filename;
+    // collect input and output file
+    cout<< "Enter the input file name" << endl;
+    cin>> inputFile;
+      //cout << "Enter the output file name" << endl;
+      //cin >> outputFile;
     fileRead(filename);
         
         //process code
 
         //generate output line
-
-        if(running){ //check if user wants to run program again
-
+        
+    //This part of the code checks if the user would like to run the program again
+    char runAgain;
+    bool notValid = true;
+	cout << "Do you want to run the simulation again? (Y/N): ";
+    cin >> runAgain;
+	cout << endl;
+	while (notValid == 1){
+        if (runAgain == 'Y'){
+            repeat = 1;
+			notValid = 0;
         }
-        //user doesn't wanna run program
-        running = false;
+		else if (runAgain == 'N'){
+			repeat = 0;
+			notValid = 0;
+		}
+		else{
+		    cout << "Invalid Input, please try agian. (Y/N): ";
+			cin >> runAgain;
+        }
     }
     
 }
